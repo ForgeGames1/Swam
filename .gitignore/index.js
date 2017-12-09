8d024643d5bd2e973fe54a09ccc1ee5b753f84f7
ForgeGames1 <34386594+ForgeGames1@users.noreply.github.com>
2017-12-09 04:05:40 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,6 +5,19 @@ var bot = new Discord.Client();
 bot.on("ready", function() {
     bot.user.setGame("Swam, !help");
     console.log("Le bot a bien ete connecte");
+    
+})
+
+bot.on('message', function (message) {
+    if (message.content === '!ping') {
+        message.channel.send('pong')
+    }
+})
+
+bot.o,('guildMemberAdd', function (member) {
+    member.createDM().then(function (channel) {
+        channel.send('Bienvenue sur le channel ' + member.displayName)
+    }).catch(console.error)
 })
 
 bot.login("Mjg5MDUwNzE1ODk1MjM0NTcw.DQzDJA.FZfPWOZ9uzXNPhMDG8Ebjp2RzG4");
